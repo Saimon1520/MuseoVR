eaff3b575dea9640483dfac1731e6b578dc34a27
HaiSeC <crissanabriah3@gmail.com>
2023-04-03 11:38:16 -0600

    jajsa

--- game.js
+++ game.js
@@ -1,361 +1,232 @@
 import * as THREE from 'three';
 
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
-const controls = new OrbitControls( camera, renderer.domElement );
+const scene = new THREE.Scene();
+
+const renderer = new THREE.WebGLRenderer();
+
+let environmentProxy = null;
+
+renderer.setSize(window.innerWidth, innerHeight);
+
+const camera = new THREE.PerspectiveCamera(40, window.innerWidth/window.innerHeight, 1, 1000);
+
+document.body.appendChild(renderer.domElement);
+
+
+camera.position.set( -3, 30, 125 );
+
+let lightCount = 6;
+let lightDistance = 3000;
+
+let lights = [];
+
+const lightValues = [
+	{colour: 0x14D14A},
+	{colour: 0xBE61CF},
+	{colour: 0x00FFFF},
+	{colour: 0x00FF00},
+	{colour: 0x16A7F5},
+	{colour: 0x90F615}
+];
+
+for (let i = 0; i < lightCount; i++){
+
+	// Positions evenly in a circle pointed at the origin
+	const light = new THREE.PointLight(0xffffff, 1);
+	let lightX = lightDistance * Math.sin(Math.PI * 2 / lightCount * i);
+	let lightZ = lightDistance * Math.cos(Math.PI * 2 / lightCount * i);
+
+	// Create a light
+	light.position.set(lightX, 0, lightZ)
+	light.lookAt(0, 0, 0)
+	scene.add(light);
+	lights.push(light);
+
+	// Visual helpers to indicate light positions
+	scene.add(new THREE.PointLightHelper(light, .5, /*0xff9900*/lightValues[i]['colour']));
+
+}
+
+
 const gltfLoader = new GLTFLoader();
 
-const camera = 
+gltfLoader.load('../assets/glb/WholeMuseum.glb', (gltf) => {
+	const root = gltf.scene;
+	root.scale.set(1,1,1);
+	scene.add(root);
 
-
-
-class Game{
-    constructor(){
-		if (window.WebGLRenderingContext) {
-			this.modes = Object.freeze({
-				NONE:   Symbol("none"),
-				PRELOAD: Symbol("preload"),
-				INITIALISING:  Symbol("initialising"),
-	
-			});
-		  } else {
-			this.mode = this.modes.NONE;
-		  }
-        
-		
-		this.container;
-		this.player = { };
-		this.camera;
-		this.scene;
-		this.renderer;
-
-        this.container = document.createElement( 'div' );
-		this.container.style.height = '100%';
-		document.body.appendChild( this.container );
-
-        const game = this;
-
-		this.mode = this.modes.PRELOAD;
-        this.clock = new THREE.Clock();
-        game.init();
-		game.animate();
-
-        window.onError = function(error){
-			console.error(JSON.stringify(error));
-		}
-    }
-    set activeCamera(object){
-		this.player.cameras.active = object;
-	}
-
-    init() {
-
-		this.mode = this.modes.INITIALISING;
-
-		this.camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 );
-		
-		this.scene = new THREE.Scene();
-		//this.scene.background = new THREE.Color( 0xa0a0a0 );
-		//this.scene.fog = new THREE.Fog( 0xa0a0a0, 200, 1000 );
-
-		let light = new THREE.HemisphereLight( 0xFFFFFF, 0x444444, 5 );
-		light.position.set( 0, 200, 0 );
-		this.scene.add( light );
-
-
-
-		// ground
-		var mesh = new THREE.Mesh( new THREE.PlaneBufferGeometry( 2000, 2000 ), new THREE.MeshPhongMaterial( { color: 0x999999, depthWrite: false } ) );
-		mesh.rotation.x = - Math.PI / 2;
-		//mesh.position.y = -100;
-		mesh.receiveShadow = true;
-		this.scene.add( mesh );
-
-		var grid = new THREE.GridHelper( 2000, 40, 0x000000, 0x000000 );
-		//grid.position.y = -100;
-		grid.material.opacity = 0.2;
-		grid.material.transparent = true;
-		this.scene.add( grid );
-
-		// player
-        const game = this;
-        var geometry = new THREE.SphereGeometry( 15*3, 32*3, 16*3 );
-        var material = new THREE.MeshBasicMaterial({
-            color: 0x9E0000,
-        });
-        var cube = new THREE.Mesh(geometry,material);
-        cube.name = "Character";
-        cube.visible= false;
-        game.scene.add(cube);
-		game.player.object = cube;
-        
-
-
-        game.joystick = new JoyStick({
-            onMove: game.playerControl,
-            game: game
-        });
-        game.createCameras();
-        game.loadEnvironment();
-		
-		
-		
-		this.renderer = new THREE.WebGLRenderer( { antialias: true } );
-		this.renderer.setPixelRatio( window.devicePixelRatio );
-		this.renderer.setSize( window.innerWidth, window.innerHeight );
-		this.renderer.shadowMap.enabled = true;
-		this.container.appendChild( this.renderer.domElement );
-			
-		window.addEventListener( 'resize', function(){ game.onWindowResize(); }, false );
-
-		// stats
-		if (this.debug){
-			this.stats = new Stats();
-			this.container.appendChild( this.stats.dom );
-		}
-	}
-
-    loadEnvironment(){
-       
-		gltfLoader.load('../assets/glb/WholeMuseum.glb', (gltf) => {
-			const root = gltf.scene;
-			root.scale.set(7, 7, 7);
-			this.scene.add(root);
-			
-			gltf.scene.traverse( function ( child ) {
-				//console.log(child)
-				if ( child.isMesh ) {
-					if (child.name.includes('main')){
-						child.castShadow = true;
-						child.receiveShadow = true;
-					}else if (child.name.includes('Cube') ){
-						//child.material.visible = false;
-						game.environmentProxy = child;
-					}}
-				
-			} )
-        })
-	}
-	
-
-    playerControl(forward, turn){
-        
-        turn = -turn;
-		
-		if (forward==0 && turn==0){
-			delete this.player.move;
-		}else{
-			this.player.move = { forward, turn }; 
-		}
-		
-	}
-
-    createCameras(){
-		
-		const back = new THREE.Object3D();
-		back.position.set(0, 150, -100);//z, altura, cercania al player
-		
-		back.quaternion.set(-0.001079297317118498, -0.9994228131639347, 0.031748701462123836, -0.031856610911161515);
-		back.parent = this.player.object;
-		
-		this.player.cameras = { back,};
-		this.activeCamera = this.player.cameras.back;	
-	}
-
-    onWindowResize() {
-		this.camera.aspect = window.innerWidth / window.innerHeight;
-		this.camera.updateProjectionMatrix();
-
-		this.renderer.setSize( window.innerWidth, window.innerHeight );
-
-	}
-    movePlayer(dt){
-		const pos = this.player.object.position.clone();
-		pos.y += 60;
-		let dir = new THREE.Vector3();
-		this.player.object.getWorldDirection(dir);
-		if (this.player.move.forward<0) dir.negate();
-		let raycaster = new THREE.Raycaster(pos, dir);
-		let blocked = false;
-		const box = this.environmentProxy;
-		if (this.environmentProxy!=undefined){ 
-			const intersect = raycaster.intersectObject(box);
-			if (intersect.length>0){
-				if (intersect[0].distance<50) blocked = true;
-			}
-		}
-		
-		if (!blocked){
-			if (this.player.move.forward>0){
-				const speed = (this.player.action=='run') ? 200 : 100;
-				this.player.object.translateZ(dt*speed);
-			}else{
-				this.player.object.translateZ(-dt*30);
-			}
-		}
-		
-		if (this.environmentProxy!=undefined){
-			//cast left
-			dir.set(-1,0,0);
-			dir.applyMatrix4(this.player.object.matrix);
-			dir.normalize();
-			raycaster = new THREE.Raycaster(pos, dir);
-
-			let intersect = raycaster.intersectObject(box);
-			if (intersect.length>0){
-				if (intersect[0].distance<50) this.player.object.translateX(50-intersect[0].distance);
-			}
-			
-			//cast right
-			dir.set(1,0,0);
-			dir.applyMatrix4(this.player.object.matrix);
-			dir.normalize();
-			raycaster = new THREE.Raycaster(pos, dir);
-
-			intersect = raycaster.intersectObject(box);
-			if (intersect.length>0){
-				if (intersect[0].distance<50) this.player.object.translateX(intersect[0].distance-50);
-			}
-			
-			//cast down
-			dir.set(0,-1,0);
-			pos.y += 200;
-			raycaster = new THREE.Raycaster(pos, dir);
-			const gravity = 30;
-
-			intersect = raycaster.intersectObject(box);
-			if (intersect.length>0){
-				const targetY = pos.y - intersect[0].distance;
-				if (targetY > this.player.object.position.y){
-					//Going up
-					this.player.object.position.y = 0.8 * this.player.object.position.y + 0.2 * targetY;
-					this.player.velocityY = 0;
-				}else if (targetY < this.player.object.position.y){
-					//Falling
-					if (this.player.velocityY==undefined) this.player.velocityY = 0;
-					this.player.velocityY += dt * gravity;
-					this.player.object.position.y -= this.player.velocityY;
-					if (this.player.object.position.y < targetY){
-						this.player.velocityY = 0;
-						this.player.object.position.y = targetY;
-					}
-				}
-			}
-		}
-	}
-	
-	animate() {
-        const game = this;
-		const dt = this.clock.getDelta();
-		
-		requestAnimationFrame( function(){ game.animate(); } );
-		
-		
-        if (this.player.move!=undefined){
-			if (this.player.move.forward!=0) this.movePlayer(dt);
-			this.player.object.rotateY(this.player.move.turn*dt);
-		}
-		
-		if (this.player.cameras!=undefined && this.player.cameras.active!=undefined){
-			this.camera.position.lerp(this.player.cameras.active.getWorldPosition(new THREE.Vector3()), 0.05);
-			this.camera.quaternion.slerp(this.player.cameras.active.getWorldQuaternion(new THREE.Quaternion()), 0.05);
-		}
-		
-		this.renderer.render( this.scene, this.camera );
-
-		if (this.stats!=undefined) this.stats.update();
-    }
-}
-
-class JoyStick{
-	constructor(options){
-		const circle = document.createElement("div");
-		circle.style.cssText = "position:absolute; bottom:35px; width:80px; height:80px; background:rgba(126, 126, 126, 0.5); border:#444 solid medium; border-radius:50%; left:50%; transform:translateX(-50%);";
-		const thumb = document.createElement("div");
-		thumb.style.cssText = "position: absolute; left: 20px; top: 20px; width: 40px; height: 40px; border-radius: 50%; background: #fff;";
-		circle.appendChild(thumb);
-		document.body.appendChild(circle);
-		this.domElement = thumb;
-		this.maxRadius = options.maxRadius || 40;
-		this.maxRadiusSquared = this.maxRadius * this.maxRadius;
-		this.onMove = options.onMove;
-		this.game = options.game;
-		this.origin = { left:this.domElement.offsetLeft, top:this.domElement.offsetTop };
-		
-		if (this.domElement!=undefined){
-			const joystick = this;
-			if ('ontouchstart' in window){
-				this.domElement.addEventListener('touchstart', function(evt){ joystick.tap(evt); });
-			}else{
-				this.domElement.addEventListener('mousedown', function(evt){ joystick.tap(evt); });
-			}
-		}
-	}
-	
-	getMousePosition(evt){
-		let clientX = evt.targetTouches ? evt.targetTouches[0].pageX : evt.clientX;
-		let clientY = evt.targetTouches ? evt.targetTouches[0].pageY : evt.clientY;
-		return { x:clientX, y:clientY };
-	}
-	
-	tap(evt){
-		evt = evt || window.event;
-		// get the mouse cursor position at startup:
-		this.offset = this.getMousePosition(evt);
-		const joystick = this;
-		if ('ontouchstart' in window){
-			document.ontouchmove = function(evt){ joystick.move(evt); };
-			document.ontouchend =  function(evt){ joystick.up(evt); };
-		}else{
-			document.onmousemove = function(evt){ joystick.move(evt); };
-			document.onmouseup = function(evt){ joystick.up(evt); };
-		}
-	}
-	
-	move(evt){
-		evt = evt || window.event;
-		const mouse = this.getMousePosition(evt);
-		// calculate the new cursor position:
-		let left = mouse.x - this.offset.x;
-		let top = mouse.y - this.offset.y;
-		//this.offset = mouse;
-		
-		const sqMag = left*left + top*top;
-		if (sqMag>this.maxRadiusSquared){
-			//Only use sqrt if essential
-			const magnitude = Math.sqrt(sqMag);
-			left /= magnitude;
-			top /= magnitude;
-			left *= this.maxRadius;
-			top *= this.maxRadius;
-		}
-        
-		// set the element's new position:
-		this.domElement.style.top = `${top + this.domElement.clientHeight/2}px`;
-		this.domElement.style.left = `${left + this.domElement.clientWidth/2}px`;
-		
-		const forward = -(top - this.origin.top + this.domElement.clientHeight/2)/this.maxRadius;
-		const turn = (left - this.origin.left + this.domElement.clientWidth/2)/this.maxRadius;
-		
-		if (this.onMove!=undefined) this.onMove.call(this.game, forward, turn);
-	}
-	
-	up(evt){
-		if ('ontouchstart' in window){
-			document.ontouchmove = null;
-			document.touchend = null;
-		}else{
-			document.onmousemove = null;
-			document.onmouseup = null;
-		}
-		this.domElement.style.top = `${this.origin.top}px`;
-		this.domElement.style.left = `${this.origin.left}px`;
-		
-		this.onMove.call(this.game, 0, 0);
-	}
-}
-document.addEventListener("DOMContentLoaded", function(){
-	const game = new Game();
-	window.game = game;//For debugging only
+	gltf.scene.traverse(function (child) {
+		if ( child.isMesh ) {
+			if (child.name.includes('main')){
+				child.castShadow = true;
+				child.receiveShadow = true;
+			}else if (child.name.includes('Cube') ){
+				//child.material.visible = false;
+				environmentProxy = child;
+			}}
+	});
 });
+
+let moveForward = false;
+let moveBackward = false;
+let moveLeft = false;
+let moveRight = false;
+
+const moveSpeed = 1;
+
+document.addEventListener('keydown', onKeyDown, false);
+document.addEventListener('keyup', onKeyUp, false);
+
+function onKeyDown(event) {
+    switch (event.keyCode) {
+        case 87: // w
+            moveForward = true;
+            break;
+        case 65: // a
+            moveRight = true;
+            break;
+        case 83: // s
+            moveBackward = true;
+            break;
+        case 68: // d
+            moveLeft = true;
+            break;
+    }
+}
+
+function onKeyUp(event) {
+    switch(event.keyCode) {
+        case 87: // w
+            moveForward = false;
+            break;
+        case 65: // a
+            moveRight = false;
+            break;
+        case 83: // s
+            moveBackward = false;
+            break;
+        case 68: // d
+            moveLeft = false;
+            break;
+    }
+}
+
+function updatePosition() {
+	if(isCursorLocked){
+    if (moveForward) {
+        camera.position.x -= moveSpeed * Math.sin(camera.rotation.y);
+        camera.position.z -= moveSpeed * Math.cos(camera.rotation.y);
+    }
+    if (moveBackward) {
+        camera.position.x += moveSpeed * Math.sin(camera.rotation.y);
+        camera.position.z += moveSpeed * Math.cos(camera.rotation.y);
+    }
+    if (moveLeft) {
+        camera.position.x -= moveSpeed * Math.sin(camera.rotation.y - Math.PI/2);
+        camera.position.z -= moveSpeed * Math.cos(camera.rotation.y - Math.PI/2);
+    }
+    if (moveRight) {
+        camera.position.x -= moveSpeed * Math.sin(camera.rotation.y + Math.PI/2);
+        camera.position.z -= moveSpeed * Math.cos(camera.rotation.y + Math.PI/2);
+    }
+
+    // Limitar la posición de la cámara
+    if (camera.position.x < -148) camera.position.x = -148;
+    if (camera.position.x > 127) camera.position.x = 127;
+    if (camera.position.z < -170) camera.position.z = -170;
+    if (camera.position.z > 170) camera.position.z = 170;
+}
+}
+
+document.addEventListener('mousemove', onDocumentMouseMove);
+
+let isCursorLocked = false;
+
+	let mouseX = 0;
+	let mouseY = 0;
+
+function onDocumentMouseMove(event) {
+	if(isCursorLocked){
+
+	mouseX = event.clientX;
+	mouseY = event.clientY;
+
+	const maxVerticalRotation = Math.PI / 5;
+	const minVerticalRotation = -Math.PI / 5;
+	//const maxHorizontalRotation = Math.PI;
+	//const minHorizontalRotation = -Math.PI;
+	
+  
+	camera.rotation.y += -(mouseX - lastMouseX);
+	camera.rotation.x += -(mouseY - lastMouseY);
+  
+	// Limit vertical rotation
+	camera.rotation.x = Math.max(minVerticalRotation, Math.min(maxVerticalRotation, camera.rotation.x));
+  
+	// Limit horizontal rotation
+	//camera.rotation.y = Math.max(minHorizontalRotation, Math.min(maxHorizontalRotation, camera.rotation.y));
+  
+	lastMouseX = mouseX;
+	lastMouseY = mouseY;
+	}
+  }
+
+  let lastMouseX = 0;
+  let lastMouseY = 0;
+	
+  camera.lookAt(scene.position);
+
+
+  // bloquear el cursor dentro de un área específica
+function lockCursor() {
+	// Pide el acceso al cursor del mouse
+	document.body.requestPointerLock();
+	  isCursorLocked = true;
+  }
+  
+  // desbloquear el cursor
+  function unlockCursor() {
+	 // Libera el acceso al cursor del mouse
+	 document.exitPointerLock();
+	  isCursorLocked = false;
+  }
+  /*
+  function updateMousePosition(event) {
+	// Verifica si el cursor está dentro de la ventana
+	if (document.pointerLockElement === document.body) {
+	  // Si se tiene acceso al cursor, actualiza la posición
+	  mouseX += event.movementX;
+	  mouseY += event.movementY;
+  
+	  // Limita la posición del mouse a los bordes de la ventana
+	  mouseX = Math.max(Math.min(mouseX, window.innerWidth), 0);
+	  mouseY = Math.max(Math.min(mouseY, window.innerHeight), 0);
+	}
+  }
+*/
+  document.body.addEventListener('click', lockCursor);
+  
+  // desactivar la restricción del cursor al presionar escape
+  document.addEventListener('keydown', (event) => {
+	if (event.key === 'Escape') {
+	  unlockCursor();
+	}
+  });
+
+
+  
+
+
+function animate() {
+	requestAnimationFrame( animate );
+
+	// required if controls.enableDamping or controls.autoRotate are set to true
+	//updateMousePosition();
+	updatePosition();
+	console.log(camera.rotation);
+	renderer.render( scene, camera );
+};
+
+animate();aw
